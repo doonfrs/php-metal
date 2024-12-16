2eb4d826cc551def75e49d29244198930b2813a8
Feras abdalrahman <doonfrs@gmail.com>
2024-12-17 01:31:52 +0300

    add testRenderHtmlChain test

--- tests/MetalTest.php
+++ tests/MetalTest.php
@@ -1,8 +1,11 @@
 <?php
 
+use function Metal\Html\a;
 use function Metal\Html\div;
 use function Metal\Html\head;
+use function Metal\Html\hr;
 use function Metal\Html\htmlPage;
+use function Metal\Html\span;
 
 use Metal\Metal;
 use PHPUnit\Framework\TestCase;
@@ -19,6 +22,26 @@ class MetalTest extends TestCase
 
         $this->assertEquals(
             '<!DOCTYPE html><html><head><title>test</title></head><div>Test</div></html>',
+            $html
+        );
+    }
+
+    public function testRenderHtmlChain()
+    {
+
+        $html = Metal::render(htmlPage(
+            head: head(title: 'test'),
+            body: div(
+                [
+                    a(href: '#', content: span('Hey!')),
+                    hr(),
+                    div('After link'),
+                ]
+            )
+        ));
+
+        $this->assertEquals(
+            '<!DOCTYPE html><html><head><title>test</title></head><div><a href="#"><span>Hey!</span></a><hr><div>After link</div></div></html>',
             $html
         );
     }
